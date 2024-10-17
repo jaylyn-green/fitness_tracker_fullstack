9fd4d0e11979c063a9fa82935bda771634bdbd1b
Jaylyn Green <jaylyng02@icloud.com>
2024-10-17 18:57:55 -0400

    Delete backend directory

--- backend/routes/myRuns.js
+++
@@ -1,9 +0,0 @@
-const { addRuns, getRuns, deleteRun } = require('../controllers/run');
-
-const router = require('express').Router();
-
-router.post('/add-runs', addRuns)
-      .get('/get-runs',getRuns)
-      .delete('/delete-runs/:id', deleteRun)
-
-module.exports = router;
