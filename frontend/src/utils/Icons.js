e6bf4321d8d21d0a4286e555af4a42bdf69c1ed0
Jaylyn Green <jaylyng02@icloud.com>
2024-10-17 18:58:03 -0400

    Delete frontend directory

--- frontend/src/utils/Icons.js
+++
@@ -1,4 +0,0 @@
-export const dashboard = <i className="fa-solid fa-house"></i>
-export const runner = <i className="fa-solid fa-person-running"></i>
-export const trend = <i className="fa-solid fa-arrow-trend-up"></i>
-export const signout = <i className="fa-solid fa-right-from-bracket"></i>
